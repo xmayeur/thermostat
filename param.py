787ee95e20f6dcfd9cedf20422c2704d74abe3d8
XY56RE <xavier.mayeur@ing.com>
2021-10-17 18:03:15 +0200

    Upd color scheme

--- param.py
+++ param.py
@@ -1,6 +1,9 @@
 # Environment
 mqtt_host = "192.168.1.10"
 mqtt_port = 1884
+# Constants
+default_night = 16
+default_day = 19
 
 
 # Language selection
@@ -9,19 +12,14 @@ class Lang:
     EN = 1
 
 
-l = Lang()
-lang = l.FR
+lang = Lang().FR
 
 # labels
 NIGHT_TEMP_TITLE = ["Temperature Nuit", "Night Temperature"][lang]
 DAY_TEMP_TITLE = ["Temperature Jour", "Day Temperature"][lang]
 
 days = [
-    ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
-    ["Mon", "Tue", "Wed", 'Thu', "Fri", "Sat", "Sun"]
-][lang]
+        ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
+        ["Mon", "Tue", "Wed", 'Thu', "Fri", "Sat", "Sun"]
+    ][lang]
 
-
-# Constants
-default_night = 16
-default_day = 19
